ff295b9c173d1d7fc19af371e2e0608430e387e1
Max <stypak1@yandex.ru>
2017-10-30 17:16:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var elem = document.querySelectorAll('td');
 for (var i = 0; i < elem.length; i++) {
-  elem[i].onclick = function addDagger() {
-    if (this.querySelector('span') == null) {
-      this.insertAdjacentHTML("afterBegin", "<span class=\"cross\"></span");
+    elem[i].onclick = function addDagger() {
+        if (this.querySelector('span') == null) {
+            this.insertAdjacentHTML("afterBegin", "<span class=\"cross\"></span");
+            //random move
+            function findEmptyField() {
+                var randomField = elem[Math.floor(Math.random() * elem.length)];
+                if (randomField.querySelector('span') == null){
+                    return randomField;
+                } else {
+                    findEmptyField();
+                }
+            }
+            var compMove = findEmptyField();
+            compMove.insertAdjacentHTML("afterBegin", "<span class=\"nought\"></span");
+        }
     }
-    while (elem[Math.floor(Math.random() * elem.length-1)].querySelector('span') == null) {
-      insertAdjacentHTML("afterBegin", "<span class=\"nought\"></span");
-    }
-  }
 }
