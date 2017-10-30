077173284ac0bd2f792224fd838cb92a3bb86512
Max <stypak1@yandex.ru>
2017-10-30 19:31:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ for (var i = 0; i < elem.length; i++) {
             }
             var compMove = findEmptyField();
             compMove.insertAdjacentHTML("afterBegin", "<span class=\"nought\"></span");
+            // end of game
+            if (document.querySelectorAll('tr:first-child td span.cross').length == 3 ||
+                document.querySelectorAll('tr:first-child td span.nought').length == 3 ||
+                document.querySelectorAll('tr:nth-child(2) td span.cross').length == 3 ||
+                document.querySelectorAll('tr:nth-child(2) td span.nought').length == 3 ||
+                document.querySelectorAll('tr:last-child td span.cross').length == 3 ||
+                document.querySelectorAll('tr:last-child td span.nought').length == 3 ||
+                document.querySelectorAll('td:first-child span.cross').length == 3 ||
+                document.querySelectorAll('td:first-child span.nought').length == 3 ||
+                document.querySelectorAll('td:nth-child(2) span.cross').length == 3 ||
+                document.querySelectorAll('td:nth-child(2) span.nought').length == 3 ||
+                document.querySelectorAll('td:last-child span.cross').length == 3 ||
+                document.querySelectorAll('td:last-child span.nought').length == 3){
+                alert("Game over");
+            }
         }
     }
 }
