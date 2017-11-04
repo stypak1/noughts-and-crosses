d7a9c716dd16af0e38a67146fc515593cede8e2a
Maks Stupak <stypak1@yandex.ru>
2017-11-04 12:20:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 var gameFields = document.querySelectorAll('td');
-var countCrossesWin = 0;
-var countNoughtWin = 0;
 for (var i = 0; i < gameFields.length; i++) {
   gameFields[i].addEventListener('click', move);
 }
@@ -10,16 +8,13 @@ function move() {
     // user move
     this.insertAdjacentHTML("afterBegin", "<span class=\"cross\"></span");
     if (checkWin() == true) {
-      alert("Crosses win");
-      countCrossesWin++;
-
+      alert("Game over");
     } else {
       //computer move
       var compMove = findEmptyField();
       compMove.insertAdjacentHTML("afterBegin", "<span class=\"nought\"></span");
       if (checkWin() == true) {
-        alert("Noughts win");
-        countNoughtWin++;
+        alert("Game over");
       }
     }
   }
@@ -35,22 +30,21 @@ function findEmptyField() {
 
 function checkWin() {
   if (document.querySelectorAll('tr:first-child td span.cross').length == 3 ||
+    document.querySelectorAll('tr:first-child td span.nought').length == 3 ||
     document.querySelectorAll('tr:nth-child(2) td span.cross').length == 3 ||
-    document.querySelectorAll('tr:last-child td span.cross').length == 3 ||
-    document.querySelectorAll('td:first-child span.cross').length == 3 ||
-    document.querySelectorAll('td:nth-child(2) span.cross').length == 3 ||
-    document.querySelectorAll('td:last-child span.cross').length == 3 ||
-    document.querySelectorAll('tr:nth-child(1) td:nth-child(1) span.cross, tr:nth-child(2) td:nth-child(2) span.cross, tr:nth-child(3) td:nth-child(3) span.cross').length == 3 ||
-    document.querySelectorAll('tr:nth-child(3) td:nth-child(1) span.cross, tr:nth-child(2) td:nth-child(2) span.cross, tr:nth-child(1) td:nth-child(3) span.cross').length == 3) {
-    return 1;
-  } else if (document.querySelectorAll('tr:first-child td span.nought').length == 3 ||
     document.querySelectorAll('tr:nth-child(2) td span.nought').length == 3 ||
+    document.querySelectorAll('tr:last-child td span.cross').length == 3 ||
     document.querySelectorAll('tr:last-child td span.nought').length == 3 ||
+    document.querySelectorAll('td:first-child span.cross').length == 3 ||
     document.querySelectorAll('td:first-child span.nought').length == 3 ||
+    document.querySelectorAll('td:nth-child(2) span.cross').length == 3 ||
     document.querySelectorAll('td:nth-child(2) span.nought').length == 3 ||
+    document.querySelectorAll('td:last-child span.cross').length == 3 ||
     document.querySelectorAll('td:last-child span.nought').length == 3 ||
+    document.querySelectorAll('tr:nth-child(1) td:nth-child(1) span.cross, tr:nth-child(2) td:nth-child(2) span.cross, tr:nth-child(3) td:nth-child(3) span.cross').length == 3 ||
     document.querySelectorAll('tr:nth-child(1) td:nth-child(1) span.nought, tr:nth-child(2) td:nth-child(2) span.nought, tr:nth-child(3) td:nth-child(3) span.nought').length == 3 ||
+    document.querySelectorAll('tr:nth-child(3) td:nth-child(1) span.cross, tr:nth-child(2) td:nth-child(2) span.cross, tr:nth-child(1) td:nth-child(3) span.cross').length == 3 ||
     document.querySelectorAll('tr:nth-child(3) td:nth-child(1) span.nought, tr:nth-child(2) td:nth-child(2) span.nought, tr:nth-child(1) td:nth-child(3) span.nought').length == 3) {
-    return 2;
+    return true;
   }
 }
